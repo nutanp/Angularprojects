59cdd9ad5c8b5995150614b3f64b76b6a4a3d7e3
nutan <nutan.pandeya@gmail.com>
2017-11-18 21:34:12 -0800

    dd

---
+++ src/app/subject/subject.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subject',
+  templateUrl: './subject.component.html',
+  styleUrls: ['./subject.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class SubjectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
